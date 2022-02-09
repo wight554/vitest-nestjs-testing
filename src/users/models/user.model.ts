848f797a1db3456dbb154019c406a9187bd167d5
Volodymyr Zhdanov <volodymyr.zhdanov@nordstrom.com>
2022-02-09 15:24:41 +0200

    add minimal reproduce for vitest

--- src/users/models/user.model.ts
+++ src/users/models/user.model.ts
@@ -2,12 +2,9 @@ import { Column, Model, Table } from 'sequelize-typescript';
 
 @Table
 export class User extends Model {
-  @Column
   firstName: string;
 
-  @Column
   lastName: string;
 
-  @Column({ defaultValue: true })
   isActive: boolean;
 }

9e4d1e5f6c4b723b972aa470aed2ef3d3986476e
Volodymyr Zhdanov <wight554@gmail.com>
2022-02-18 23:08:12 +0200

    Add fixed version with rollup and swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,49 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { createFilter } from "@rollup/pluginutils";
+import { transform } from "@swc/core";
+import { Plugin } from "vite";
+
+export const queryRE = /\?.*$/;
+export const hashRE = /#.*$/;
+export const cleanUrl = (url: string) => url.replace(hashRE, "").replace(queryRE, "");
+
+export function RollupPluginSwc(): Plugin {
+  const filter = createFilter(/\.(tsx?|jsx)$/, /\.js$/);
+
+  return {
+    name: "rollup-plugin-swc",
+    async transform(code, id) {
+      if (filter(id) || filter(cleanUrl(id))) {
+        const result = await transform(code, {
+          jsc: {
+            target: "es2019",
+            parser: {
+              syntax: "typescript",
+              decorators: true,
+            },
+            transform: {
+              legacyDecorator: true,
+              decoratorMetadata: true,
+            },
+          },
+          filename: id,
+        });
+        return {
+          code: result.code,
+          map: result.map,
+        };
+      }
+    },
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: false,
+  plugins: [RollupPluginSwc()],
   test: {
-    environment: 'node',
+    environment: "node",
   },
 });
